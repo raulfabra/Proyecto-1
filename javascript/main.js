1ca54d8538b0c054f4dbaf5465b75592c2217e73
Raul Fabra <fabraraul@yahoo.com>
2023-01-31 17:40:06 +0100

    establecidos los disparos enemigos

--- javascript/main.js
+++ javascript/main.js
@@ -5,12 +5,16 @@ const background = new Image();
 background.src = "imagenes/bkg.png";
 
 
+
 let navePlayer = document.createElement("img");
 navePlayer.src = "imagenes/player1.png";
 
 
-let naveEnemiga = document.createElement ("img");
-naveEnemiga.src = "imagenes/player1.png";
+let naveEnemiga001 = document.createElement ("img");
+naveEnemiga001.src = "imagenes/enemigo1.png";
+
+let naveEnemiga003 = document.createElement ("img");
+naveEnemiga003.src = "imagenes/enemigo3.png"
 
 
 
@@ -38,31 +42,92 @@ window.addEventListener("load",() =>{
             if (this.posY > (canvas.height-this.height)) return this.posY = canvas.height - this.height;
         }
         
+        bala(){
 
-        shot(){}
+        }
 
     }
 
     class Enemies {
-        constructor (){
+        constructor (archivo){
             this.posX = canvas.width +10;
-            this.posY = Math.floor(Math.random() * canvas.height)
-            this.width = 100;
-            this.height = 100;
-            this.velocidadPos = 5;
+            this.posY = Math.floor(Math.random() * (canvas.height - 100))
+            this.width = 90;
+            this.height = 90;
+            this.velocidadPos = 2;
+            /* this.fire = []; */
+            this.imagen = archivo;
             //this.velocidadDisp = 'valor';
         }
 
         print(){
-            ctx.drawImage(naveEnemiga, this.posX, this.posY, this.width, this.height)
+            ctx.drawImage(this.imagen, this.posX, this.posY, this.width, this.height);
+            //pintar balas tambien
+            
+
         }
 
         move(){
-            this.posX -= this.velocidadPos
+            this.posX -= this.velocidadPos;
+            /* this.fire.forEach(bala => { //Recorrer array balas y moure
+                bala.move()
+            })  */
         }
 
-        shot(){}
+        /*crearBala(){
+            let bala = new Shot(this.posX, this.posY + (this.height/2));
+            //this.fire.push(bala);
 
+            /*this.fire.forEach(bala => {
+                bala.move()
+                bala.print()
+               // bala.clear();
+            }) */
+        //}
+
+        /* clear(){
+            setInterval(() => {
+                ctx.clearRect(this.posX, this.posY + (this.height/2))
+            }, 20);
+        }*/
+
+    }
+    class Shot {
+        constructor(x,y){
+            this.posX = x
+            this.posY = y
+            this.radio = 5
+            this.startAngle = 0
+            this.endAngle = 2 * Math.PI
+            this.velocidadX = 5
+            this.velocidadY = 25
+            this.contadorBalas = 0;
+        }
+        print(){
+            
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.fill(ctx.arc(this.posX, this.posY, this.radio, this.startAngle, this.endAngle));
+            ctx.stroke();
+            let count = 50;
+            let identificador = setInterval(() => {
+                ctx.beginPath();
+                ctx.fillStyle = "red";
+                ctx.fill(ctx.arc((this.posX + count), this.posY, this.radio, this.startAngle, this.endAngle));
+                ctx.stroke();
+                count += 50
+            },1000)
+
+            setTimeout(()=>{
+                clearInterval(identificador)
+            },3000)
+        } 
+
+        move(){
+            this.posX -= this.velocidadX
+            //this.posY -= this.velocidadY
+
+        }
     }
 
     class Game{
@@ -70,19 +135,27 @@ window.addEventListener("load",() =>{
             this.player1 = new Player();
             this.player1.print();
             this.enemies = [];
+            this.fire = [];
+            this.firePlayer = [];
             this.score = 0;
             this.intervalId = undefined;
             this.iteracion = 0;
+            this.contadorBalas = 0;
         }
 
         start(){
             if(this.intervalId == undefined) {
                 this.intervalId = setInterval(()=>{
                   this.iteracion ++;
+                  //this.contadorBalas ++;
+                  //this.contadorBalas++;
+                  //console.log(this.iteracion)
+                  //console.log(this.contadorBalas)
+                  
                   //borra
                   this.clear();
                   //recalcula + genera obstaculos
-                  this.recalcular();
+                  this.recalcular();                  
                   //pinta
                   this.print();
                 }, 20);
@@ -103,36 +176,87 @@ window.addEventListener("load",() =>{
             this.enemies.forEach((enemie) => {
                 enemie.print();
             })
+            this.fire.forEach(bala => {
+                bala.print()
+                
+            })
+            
+
+
         }
 
         recalcular(){
-            if (this.iteracion == 70){
-                let enemigo1 = new Enemies()
-                this.enemies.push(enemigo1)
+
+            /* if(this.interacion % 20){
+                this.enemies.forEach(enemie => {
+                    let bala011 = new Shot(enemie.posX, enemie.posY);
+                    this.fire.push(bala011)
+                }) 
+                
+            }*/
+            if (this.iteracion == 200){
+                let enemigo1 = new Enemies(naveEnemiga001);
+                this.enemies.push(enemigo1);
+                let bala1 = new Shot(enemigo1.posX, enemigo1.posY);
+                this.fire.push(bala1)
+                
+                /* this.fire.forEach((bala) => {
+                    bala.crearBala()
+                    this.fire.push(bala)
+
+                }) */
+
+            }
+            
+            if(this.iteracion == 350){
+                let enemigo3 = new Enemies(naveEnemiga003);
+                this.enemies.push(enemigo3);
+                let bala3 = new Shot(enemigo3.posX, enemigo3.posY);
+                this.fire.push(bala3)
+                /* this.fire.forEach((bala) => {
+                    bala.crearBala()
+                }) */
                 this.iteracion = 0
             }
+            
             this.enemies.forEach((enemie) => {
                 enemie.move();
-            
+
                 if (!(
-                    this.player1.posX + this.player1.width < enemie.posX ||
-                    this.player1.posX > enemie.posX + enemie.width ||
-                    this.player1.posY > enemie.posY + enemie.height ||
-                    this.player1.posY + this.player1.height < enemie.posY
+                    this.player1.posX + this.player1.width < enemie.posX + 20||
+                    this.player1.posX - 8 > enemie.posX + enemie.width ||
+                    this.player1.posY + 20 > enemie.posY + enemie.height ||
+                    this.player1.posY + this.player1.height <= enemie.posY + 20
                 )){ 
                     this.end()
                 }
             })
+            this.fire.forEach(bala => {
+                bala.move()
+            })
+            
+            /* if (this.contadorBalas == 500){
+                this.enemies.forEach((enemie) => {
+                    enemie.bala();
+                })
+                this.contadorBalas = 0;
+              } */
+            
         }
 
-
+        score(){
+            let record = document.getElementById('scoreKeeper').innerHTML;
+            //if (){ return record += 100}
+        };
     }
+
+
 
     let partida = new Game();
 
     partida.start();
 
-    document.getElementsByTagName("body")[0].addEventListener("keydown",(event) => {
+    document.getElementsByTagName("body")[0].addEventListener("keydown",(event) => { //console.log(event.offsetX)
         switch(event.key){
           case "ArrowUp":
             partida.player1.moveUp()
@@ -140,7 +264,11 @@ window.addEventListener("load",() =>{
           case "ArrowDown":
             partida.player1.moveDown()
             break;
+            case "a":
+            partida.player1.shot()
+            break;
         }
       })
+
 
 })
