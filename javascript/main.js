a66320593b9ab85fe0c90226127c0d00176c0117
Ruben Lopez <rubenlopsol86@gmail.com>
2023-02-01 11:41:08 +0100

    codigo raul

--- javascript/main.js
+++ javascript/main.js
@@ -1,243 +1,292 @@
-
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
-
-// const audio = new Audio("imagenes/bso.mp3");
-// audio.play();
-
+​
 const background = new Image();
 background.src = "imagenes/bkg.png";
-
+​
+​
+​
 let navePlayer = document.createElement("img");
 navePlayer.src = "imagenes/player1.png";
-
-let naveEnemiga1 = document.createElement("img");
-naveEnemiga1.src = "imagenes/enemigo1.png";
-
-let naveEnemiga2 = document.createElement("img");
-naveEnemiga2.src = "imagenes/enemigo2.png";
-
-let naveEnemiga3 = document.createElement("img");
-naveEnemiga3.src = "imagenes/enemigo3.png";
-
-let naveEnemiga4 = document.createElement("img");
-naveEnemiga4.src = "imagenes/enemigo4.png";
-
-window.addEventListener("load", () => {
-   class Player1 {
-     constructor() {
-        this.posX = 55;
-        this.posY = 460;
-        this.width = 100;
-        this.height = 100;
-        this.velocidadPos = 30;
-        //this.velocidadDisp = 'valor';
-      }
-      print() {
-        ctx.drawImage(
-          navePlayer,
-          this.posX,
-          this.posY,
-          this.width,
-          this.height
-        );
-      }
-
-      moveUp() {
-        this.posY -= this.velocidadPos;
-        if (this.posY < 0) return (this.posY = 0);
-      }
-      moveDown() {
-        this.posY += this.velocidadPos;
-        if (this.posY > canvas.height - this.height)
-          return (this.posY = canvas.height - this.height);
-      }
-
-      shotprint1() {
-        //+++++++++++++++++++++++++++++++++++llegada impresion balaen player
-        ctx.beginPath();
-        ctx.fillStyle = "blue";
-        ctx.fill(
-          ctx.arc(
-            this.posX,
-            this.posY,
-            this.radio,
-            this.startAngle,
-            this.endAngle,
-            this.velocidadX,
-            this.velocidadY
-          )
-        );
-        ctx.stroke();
-      }
+​
+​
+let naveEnemiga001 = document.createElement ("img");
+naveEnemiga001.src = "imagenes/enemigo1.png";
+​
+let naveEnemiga003 = document.createElement ("img");
+naveEnemiga003.src = "imagenes/enemigo3.png"
+​
+​
+​
+​
+window.addEventListener("load",() =>{
+    class Player {
+        constructor (){
+            this.width = 100;
+            this.height = 100;
+            this.posX = this.width;
+            this.posY = (canvas.height / 2) - (this.height / 2);
+            this.velocidadPos = 30;
+            //this.velocidadDisp = 'valor';
+        }
+        print(){
+            ctx.drawImage (navePlayer, this.posX, this.posY, this.width, this.height)
+        }
+​
+        moveUp(){
+            this.posY -= this.velocidadPos;
+            if (this.posY < 0) return this.posY = 0;
+        }
+        moveDown(){
+            this.posY += this.velocidadPos;
+            if (this.posY > (canvas.height-this.height)) return this.posY = canvas.height - this.height;
+        }
+        
+        dirShotPlayer(){
+            this.posX += 5
+        }
+​
     }
-
-    class Enemie2 {
-      constructor() {
-        this.posX = canvas.width + 250;
-        this.posY = Math.floor(Math.random * canvas.height - 100);
-        this.width = 80;
-        this.height = 80;
-        this.velocidadPos = Math.random() * 1 + 1;
-
-        //this.velocidadDisp = 'valor';
-      }
-
-      print() {
-        ctx.drawImage(
-          naveEnemiga1,
-          this.posX,
-          this.posY,
-          this.width,
-          this.height
-        );
-      }
-
-      move() {
-        this.posX -= this.velocidadPos;
-      }
-
-      shotprint2() {
-        //+++++++++++++++++++++++++++++++++++llegada impresion balaen emigo
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.fill(
-          ctx.arc(
-            this.posX,
-            this.posY,
-            this.radio,
-            this.startAngle,
-            this.endAngle,
-            this.velocidadX,
-            this.velocidadY
-          )
-        );
-        ctx.stroke();
-      }
+​
+    class Enemies {
+        constructor (archivo){
+            this.posX = canvas.width +10;
+            this.posY = Math.floor(Math.random() * (canvas.height - 100))
+            this.width = 90;
+            this.height = 90;
+            this.velocidadPos = 2;
+            /* this.fire = []; */
+            this.imagen = archivo;
+            //this.velocidadDisp = 'valor';
+        }
+​
+        print(){
+            ctx.drawImage(this.imagen, this.posX, this.posY, this.width, this.height);
+            //pintar balas tambien
+            
+​
+        }
+​
+        move(){
+            this.posX -= this.velocidadPos;
+            /* this.fire.forEach(bala => { //Recorrer array balas y moure
+                bala.move()
+            })  */
+        }
+​
+        /*crearBala(){
+            let bala = new Shot(this.posX, this.posY + (this.height/2));
+            //this.fire.push(bala);
+​
+            /*this.fire.forEach(bala => {
+                bala.move()
+                bala.print()
+               // bala.clear();
+            }) */
+        //}
+​
+        /* clear(){
+            setInterval(() => {
+                ctx.clearRect(this.posX, this.posY + (this.height/2))
+            }, 20);
+        }*/
+​
     }
-
-    class Bullets {
-      constructor(posX, posY) {
-        this.posX = posX;
-        this.posY = posY;
-        this.radio = 5;
-        this.startAngle = 0;
-        this.endAngle = 2 * Math.PI;
-        this.velocidadX = 5;
-        this.velocidadY = 25;
-      }
-
-      move(bollet001, bollet002) {
-        if (bollet001) {
-          this.posX += this.velocidadPos;
+    class Shot {
+        constructor(x,y){
+            this.posX = x
+            this.posY = y
+            this.radio = 5
+            this.startAngle = 0
+            this.endAngle = 2 * Math.PI
+            this.velocidadX = 5
+            this.velocidadY = 25
+            //this.contadorBalas = 0;
         }
-        if (bollet002) {
-          this.posX -= this.velocidadX;
+        printBalaPlayer(){
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.fill(ctx.arc(this.posX, this.posY, this.radio, this.startAngle, this.endAngle));
+            ctx.stroke();
         }
-      }
+​
+        print(){
+            
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.fill(ctx.arc(this.posX, this.posY, this.radio, this.startAngle, this.endAngle));
+            ctx.stroke();
+​
+           /*  let count = 50;
+            let identificador = setInterval(() => {
+                ctx.beginPath();
+                ctx.fillStyle = "red";
+                ctx.fill(ctx.arc((this.posX + count), this.posY, this.radio, this.startAngle, this.endAngle));
+                ctx.stroke();
+                count += 100
+            },1000)
+​
+            setTimeout(()=>{
+                clearInterval(identificador)
+            },3000) */
+        } 
+​
+        moveLeft(){
+            this.posX -= this.velocidadX
+            //this.posY -= this.velocidadY
+        }
+​
+        moveRight(){
+            setInterval(()=>{
+                this.posX += this.velocidadX;
+            },33)   
+        }
     }
-
-    class Game {
-      constructor() {
-        this.player1 = new Player1();
-        this.player1.print();
-        this.enemie2 = [];
-        this.bulletArrEnemy = []; //+++++++++++++++++++++++array bullets enemy
-        this.bulletArrPlayer = []; //+++++++++++++++++++++++array bullets player
-        this.score = 0;
-        this.intervalId = undefined;
-        this.iteracion = 0;
-      }
-
-      start() {
-        if (this.intervalId == undefined) {
-          this.intervalId = setInterval(() => {
-            this.iteracion++;
-            //borra
-            this.clear();
-            //recalcula + genera obstaculos
-            this.recalcular();
-            //pinta
-            this.print();
-          }, 20);
+​
+    class Game{
+        constructor (){
+            this.player1 = new Player();
+            this.player1.print();
+            //this.bala002 = new Shot(this.player1.posX, this.player1.posY)
+            this.enemies = [];
+            this.fire = [];
+            this.firePlayer = [];
+            this.score = 0;
+            this.intervalId = undefined;
+            this.iteracion = 0;
+            this.contadorBalas = 0;
         }
-      }
-
-      end() {
-        if (this.intervalId) clearInterval(this.intervalId);
-      }
-
-      clear() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-      }
-
-      print() {
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        this.player1.print();
-        this.enemie2.forEach((enemie02) => {
-          //++++++++++++++++++++++++forEache print signal
-          enemie02.print();
-        });
-        this.bulletArrPlayer.forEach((bullet01) => {
-          bullet01.shotprint1();
-        });
-
-        this.bulletArrEnemy.forEach((bullet02) => {
-          bullet02.shotprint2();
-        });
-      }
-
-      recalcular() {
-        if (this.iteracion % 25 == 0) {
-          let enemigo2 = new Enemie2();
-          this.enemie2.push(enemigo2);
-          this.iteracion = 0;
+​
+        start(){
+            if(this.intervalId == undefined) {
+                this.intervalId = setInterval(()=>{
+                  this.iteracion ++;
+                  //this.contadorBalas ++;
+                  //this.contadorBalas++;
+                  //console.log(this.iteracion)
+                  //console.log(this.contadorBalas)
+                  
+                  //borra
+                  this.clear();
+                  //recalcula + genera obstaculos
+                  this.recalcular();                  
+                  //pinta
+                  this.print();
+                }, 33);
+              }
         }
-        if (this.iteration % 10 == 0) {
-          let bulletEnemy002 = new Bullets(); //++++++crear bullet+ push al array/enemy
-          this.bulletArrEnemy.push(bulletEnemy002);
-          let bulletPlayer001 = new Bullets(); // +++++++++crear bullet+ push to array/player
-          this.bulletArrPlayer.push(bulletPlayer001);
+​
+        end(){
+            if(this.intervalId) clearInterval(this.intervalId);
         }
-
-        this.enemie2.forEach((enemie002) => {
-          enemie002.move();
-        });
-
-        // this.bulletarr.forEach((bollet001) => {
-        //   bollet001.move(bollet001); //+++++++++++++++++++++++señal movimiento
-        // });
-        // this.bulletarr.forEach((bollet002) => {
-        //   bollet002.move(bollet002); //+++++++++++++++++++++++señal movimiento
-        // });
-
-        //   if (!(
-        //     this.player1.posX + this.player1.width <= Enemie1.posX + 20||
-        //     this.player1.posX > Enemie1.posX + Enemie1.width ||
-        //     this.player1.posY > Enemie1.posY + Enemie1.height ||
-        //     this.player1.posY + this.player1.height <= Enemie1.posY + 20
-        // )){
-        //     this.end()
-        // }
-      }
+​
+        clear(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+        }
+​
+        print(){
+            ctx.drawImage(background,0,0,canvas.width, canvas.height)
+            this.player1.print()
+            this.bala002.print()
+            this.enemies.forEach((enemie) => {
+                enemie.print();
+            })
+            this.fire.forEach(bala => {
+                bala.print()
+                
+            })
+​
+        }
+​
+        recalcular(){
+            //this.firePlayer.push(this.bala002);
+​
+            /* if(this.interacion % 20){
+                let bala001 = new Shot(this.player1.posX, this.player1.posY)
+                this.firePlayer.push(bala001)
+                
+            }  */
+            if (this.iteracion == 200){
+                let enemigo1 = new Enemies(naveEnemiga001);
+                this.enemies.push(enemigo1);
+               /*  let bala1 = new Shot(enemigo1.posX, (enemigo1.posY + (enemigo1.height / 2)));
+                this.fire.push(bala1) */
+                
+                /* this.fire.forEach((bala) => {
+                    bala.crearBala()
+                    this.fire.push(bala)
+​
+                }) */
+​
+            }
+            
+            if(this.iteracion == 350){
+                let enemigo3 = new Enemies(naveEnemiga003);
+                this.enemies.push(enemigo3);
+               /*  let bala3 = new Shot(enemigo3.posX, (enemigo3.posY + (enemigo3.height / 2)));
+                this.fire.push(bala3) */
+                /* this.fire.forEach((bala) => {
+                    bala.crearBala()
+                }) */
+                this.iteracion = 0
+            }
+            
+            this.enemies.forEach((enemie) => {
+                enemie.move();
+​
+                if (!(
+                    this.player1.posX + this.player1.width < enemie.posX + 25||
+                    this.player1.posX - 15 > enemie.posX + enemie.width ||
+                    this.player1.posY + 25 > enemie.posY + enemie.height ||
+                    this.player1.posY + this.player1.height <= enemie.posY + 25
+                )){ 
+                    this.end()
+                }
+            })
+            this.fire.forEach(bala => {
+                bala.moveLeft()
+            })
+​
+            this.firePlayer.forEach(bala => {
+                bala.moveRight()
+            })
+            
+            /* if (this.contadorBalas == 500){
+                this.enemies.forEach((enemie) => {
+                    enemie.bala();
+                })
+                this.contadorBalas = 0;
+              } */
+            
+        }
+​
+        score(){
+            let record = document.getElementById('scoreKeeper').innerHTML;
+            //if (){ return record += 100}
+        };
     }
-
+​
+​
+​
     let partida = new Game();
-
+​
     partida.start();
-
-    document
-      .getElementsByTagName("body")[0]
-      .addEventListener("keydown", (event) => {
-        switch (event.key) {
+​
+    document.getElementsByTagName("body")[0].addEventListener("keydown",(event) => { //console.log(event.offsetX)
+        console.log(event.key)
+        switch(event.key){
           case "ArrowUp":
-            partida.player1.moveUp();
+            partida.player1.moveUp()
             break;
           case "ArrowDown":
-            partida.player1.moveDown();
+            partida.player1.moveDown()
+            break;
+            case " ":
+                let bala001 = new Shot(partida.player1.posX, partida.player1.posY)
+                partida.firePlayer.push(bala001)        
             break;
         }
-      });
-  });
-
-
+      })
+​
+​
+})
